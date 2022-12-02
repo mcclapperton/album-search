34898cdb077f86d7f76da6353553a6386c0423ba
Maris <mcclapperton@gmail.com>
2022-12-01 21:02:32 -0700

    initial commit

--- react-albums/src/components/LiveSearch.js
+++ react-albums/src/components/LiveSearch.js
@@ -1,5 +1,5 @@
-import React, { Fragment, useState } from "react";
-
+import React, { Fragment, useState, useEffect } from "react";
+import axios from "axios";
 import SearchBar from "components/SearchBar";
 import Results from "components/Results";
 
@@ -7,13 +7,19 @@ export default function LiveSearch(props) {
   const [term, setTerm] = useState("");
   const [results, setResults] = useState([]);
 
+  useEffect(() => {
+    const testURL = `https://itunes.apple.com/search?term=beatles&country=CA&media=music&entity=album&attribute=artistTerm`;
+    axios.get(testURL).then((response) => {
+      console.log(response);
+    });
+  }, []);
   return (
     <Fragment>
       <header className="logo">
         <img src="images/brand.png" alt="Brand" />
       </header>
       <main>
-        <SearchBar onSearch={term => setTerm(term)} />
+        <SearchBar onSearch={(term) => setTerm(term)} />
         <Results results={results} />
       </main>
     </Fragment>
